ed3dde97cc1393ce584b7c3dc45e290309a93d24
staand <staand@ukr.net>
2012-10-20 13:22:52 +0300

    Initial commit.

---
+++ Skype-Calling-Bot/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Skype_Calling_Bot
+{
+    static class EntryPoint
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+    }
+}
